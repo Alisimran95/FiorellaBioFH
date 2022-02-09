64838fc3016dbdefc92973f4f91054523b30f6fe
Alisimran95 <alisimranem@code.edu.az>
2022-02-09 16:47:52 +0400

    homework

---
+++ FiorellaBackToFrontProject/FiorellaBackToFrontProject/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FiorellaBackToFrontProject.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiorellaBackToFrontProject.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _dbContext;
+
+        public HeaderViewComponent(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var bio = await _dbContext.Bios.SingleOrDefaultAsync();
+            return View(bio);
+        }
+    }
+}
